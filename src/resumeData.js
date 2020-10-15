883ee06d5e144661d9e2f415a6b0a6d69ae207bf
rajan sethi <rajan.sethi36@yahoo.in>
2020-10-15 15:33:54 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,117 @@
+let resumeData = {
+  "imagebaseurl":"https://techonoeticsclub.github.io/",
+  "name": "Rajan Sethi",
+  "role": "Data Scientist aspirant",
+  "linkedinId":"https://www.linkedin.com/rajan-sethi-49aa4792/",
+  "roleDescription": " I like dabbling in various parts of software development and machine learning. Currently, I'm having 5 years of experience and pursuing executive education PGP - ML/AL from Texas University, Austin",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/rajan-sethi-49aa4792/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://eportfolio.greatlearning.in/rajan-sethi",
+        "className":"fa fa-github"
+      }
+    ],
+  "aboutme":"I've five years of experience in IT industry and I've worked on various technologies and frameworks like Automation, React.JS, Java, SpringBoot",
+  "address":"India",
+  "website":"https://eportfolio.greatlearning.in/rajan-sethi",
+  "education":[
+    {
+      "UniversityName":"The University of Texas, Austin",
+      "specialization":"Machine Learning/Artificial Intelligence and Mathematics",
+      "MonthOfPassing":"Mar",
+      "YearOfPassing":"2021",
+      "Achievements":"Researched on COVID-19 using Mathematical Model which got highlighted in ML/AI conference."
+    },
+    {
+      "UniversityName":"IP University",
+      "specialization":"Computer Science Engineering",
+      "MonthOfPassing":"Jul",
+      "YearOfPassing":"2015",
+      "Achievements":"Research paper on Advance Emergency Braking System"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Srijan Technology",
+      "Client":"McKinsey & Company",
+      "specialization":"Senior Software Engineer L2",
+      "MonthOfLeaving":"Nov'18 - currently working",
+      "YearOfLeaving":"",
+      "Achievements":"Client - McKinsey & Company"
+    },
+    {
+      "CompanyName":"Bharti Airtel",
+      "specialization":"Software Engineer",
+      "MonthOfLeaving":"July",
+      "YearOfLeaving":"2018",
+      "Achievements":"Application - myAirtel App"
+    },
+    {
+      "CompanyName":"Dell International Services",
+      "specialization":"Software Engineer",
+      "MonthOfLeaving":"Jan'16 - Aug'17",
+      "YearOfLeaving":"",
+      "Achievements":"Application - Gentera"
+    }
+  ],
+  "skillsDescription":"I have worked on various technologies and domains : ",
+  "skills":[
+    {
+      "skillname":"Neural Network"
+    },
+    {
+      "skillname":"Supervised Learing"
+    },
+    {
+      "skillname":"Unsupervised learning"
+    },
+    {
+      "skillname":"Hypothesis Analysis/Testing"
+    },
+    {
+      "skillname":"Java"
+    },
+    {
+      "skillname":"SQL"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"Product Recommendation Systems",
+      "description":"Skills and Tools - Collaborative Filtering, Recommender Systems, Python",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"Predict the Bankruptcy with Unsupervised Learning Techniques",
+      "description":"Skills and Tools - PCA, Clustering, Data Visualization",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"Diagnosing Parkinson's disease using Random Forests",
+      "description":"Skills and Tools - EDA, Logistic regression, Decision Trees",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"Identifying potential customers for loans",
+      "description":"Skills and Tools - Logistic Regression, KNN, Classification",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
